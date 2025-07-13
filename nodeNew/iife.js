ec22042cea37761dacf96fda3b147abe428afd2d
JustFr4nck <francyperry07@gmail.com>
2025-07-13 17:16:37 +0200

    upgrade next week

---
+++ nodeNew/iife.js
@@ -0,0 +1,15 @@
+
+//module wrapper
+
+
+(function(message){
+
+    const superHero = "batman";
+    console.log(message, superHero);
+})("Hey");
+
+(function(message){
+
+    const superHero = "superman";
+    console.log(message, superHero);
+})("Ciao");
